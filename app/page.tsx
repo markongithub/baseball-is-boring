15204254f1b8499c852915c81920cfc6088c7331
mark on github <markongithub@users.noreply.github.com>
2025-04-10 17:48:26 -0400

    the page works too

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,14 @@ import { doItAllLive } from "./lib/baseball";
 
 export default async function Home(props: {
   searchParams: Promise<{
-    date: string;
-    venue: string;
     gameID: number;
   }>;
 }) {
   const searchParams = await props.searchParams;
-  if (!searchParams.date) {
-    throw new Error("you didn't specify a date");
-  }
-  if (!searchParams.venue) {
-    throw new Error("you didn't specify a venue");
-  }
   if (!searchParams.gameID) {
     throw new Error("you didn't specify a gameID");
   }
-  const output = await doItAllLive(searchParams.date, searchParams.venue, searchParams.gameID);
+  const output = await doItAllLive(searchParams.gameID);
 
   return (JSON.stringify(output));
 }
