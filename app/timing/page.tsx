f37adbb2246c0e0ff23af16e86d2be9843346d62
mark on github <markongithub@users.noreply.github.com>
2026-03-17 14:45:57 -0400

    Add comments about what we want here

--- app/timing/page.tsx
+++ app/timing/page.tsx
@@ -10,6 +10,8 @@ export default async function Home(props: {
     throw new Error("you didn't specify a gameID");
   }
   const output = await doItAllLive(searchParams.gameID);
-
+  // Away team 5, Home team 4
+  // Top 5th, 1 out
+  // At this pace, the game will end in XXhYYm (at hh:mm) if the bottom of the 9th is not played, and in XXhYYm (at hh:mm) if it is.
   return (JSON.stringify(output));
 }
